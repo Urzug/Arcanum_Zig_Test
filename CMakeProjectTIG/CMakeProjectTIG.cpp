486eeac6f6ae62bf527f643fea7e61e8f32c3ef2
Urzug <85928353+Urzug@users.noreply.github.com>
2025-04-20 23:36:29 +0200

    Add files via upload

---
+++ CMakeProjectTIG/CMakeProjectTIG.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProjectTIG.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjectTIG.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
